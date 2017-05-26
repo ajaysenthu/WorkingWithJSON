05b2fe92e813216e625928537220b999bc4f8b11
Ajay <ajaysenthu13@gmail.com>
2017-05-26 16:40:55 +0530

    Initial Commit

---
+++ WorkingWithJSON/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WorkingWithJSON
+//
+//  Created by Ajay on 5/26/17.
+//  Copyright © 2017 AJ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WorkingWithJSON/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WorkingWithJSON
+//
+//  Created by Ajay on 5/26/17.
+//  Copyright © 2017 AJ Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
